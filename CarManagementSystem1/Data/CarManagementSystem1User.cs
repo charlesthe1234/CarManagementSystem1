8c345dd19b2f60a192ab02b9ef7343b3949693f6
charlesthe1234 <mhr2133226041@gmail.com>
2025-12-03 09:57:28 +0800

    Week6.7

---
+++ CarManagementSystem1/Data/CarManagementSystem1User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CarManagementSystem1.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class CarManagementSystem1User : IdentityUser
+    {
+    }
+}
